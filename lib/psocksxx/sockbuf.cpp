fa29612e621c90355aeb89953cfca8700b97a10b
Uditha Atukorala <fake@thewireframe>
2013-08-06 22:08:06 +0100

    adding skeleton socket buffer class

---
+++ lib/psocksxx/sockbuf.cpp
@@ -0,0 +1,28 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include "sockbuf.h"
+
+
+namespace psocksxx {
+
+
+} /* end of namespace psocksxx */
+
+
---
+++ lib/psocksxx/sockbuf.h
@@ -0,0 +1,34 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef PSOCKSXX_SOCKBUF_H
+#define PSOCKSXX_SOCKBUF_H
+
+#include <streambuf>
+
+namespace psocksxx {
+
+	class sockbuf : public std::streambuf {
+
+	};
+
+} /* end of namespace psocksxx */
+
+#endif /* !PSOCKSXX_SOCKBUF_H */
+
