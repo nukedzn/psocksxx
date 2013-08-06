157c43f2bf168a3d305a0640f245259b29981626
Uditha Atukorala <fake@thewireframe>
2013-08-06 23:34:39 +0100

    adding skeleton i/o socket steam classes

---
+++ lib/psocksxx/isockstream.h
@@ -0,0 +1,35 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef PSOCKSXX_ISOCKSTREAM_H
+#define PSOCKSXX_ISOCKSTREAM_H
+
+#include <istream>
+
+
+namespace psocksxx {
+
+	class isockstream : public std::istream {
+
+	}
+
+} /* end of namespace psocksxx */
+
+#endif /* !PSOCKSXX_ISOCKSTREAM_H */
+
