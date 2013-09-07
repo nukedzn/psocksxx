acad2d7693c9b59e328dcca8ca38113e9e477ed9
Uditha Atukorala <fake@thewireframe>
2013-09-07 10:24:08 +0100

    unit tests for local socket stream class

---
+++ test/lsockstream_test.cpp
@@ -0,0 +1,49 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include "lsockstream_test.h"
+
+#include <psocksxx/lsockstream.h>
+
+
+// register the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( lsockstream_test );
+
+// use namespace psocksxx
+using namespace psocksxx;
+
+
+void lsockstream_test::test_constructors() {
+
+	// default constructor
+	CPPUNIT_ASSERT_NO_THROW( lsockstream l );
+
+}
+
+
+void lsockstream_test::test_connect_fail() {
+
+	// local socket stream
+	lsockstream l;
+
+	// this should throw a file not found error
+	CPPUNIT_ASSERT_THROW ( l.connect( LOCAL_SOCK_PATH ), sockexception );
+
+}
+
---
+++ test/lsockstream_test.h
@@ -0,0 +1,51 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef LSOCKSTREAM_TEST_H
+#define LSOCKSTREAM_TEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#ifndef LOCAL_LISTENER_SOCK_PATH
+#define LOCAL_LISTENER_SOCK_PATH    "/tmp/psocksxx.listener.sock"
+#endif
+
+#ifndef LOCAL_SOCK_PATH
+#define LOCAL_SOCK_PATH             "/tmp/psocksxx.sock"
+#endif
+
+
+class lsockstream_test : public CppUnit::TestFixture {
+
+	// setup the test suite
+	CPPUNIT_TEST_SUITE( lsockstream_test );
+	CPPUNIT_TEST( test_constructors );
+	CPPUNIT_TEST( test_connect_fail );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+
+	void test_constructors();
+	void test_connect_fail();
+
+
+};
+
+#endif /* !LSOCKSTREAM_TEST_H */
+
