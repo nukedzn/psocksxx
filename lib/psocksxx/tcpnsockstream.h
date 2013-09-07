6fa33864a3c0cd52e4190acd9aea1d6da7d03b70
Uditha Atukorala <fake@thewireframe>
2013-09-07 12:52:40 +0100

    adding template class for TCP/IP communications

---
+++ lib/psocksxx/tcpnsockstream.h
@@ -0,0 +1,41 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef PSOCKSXX_TCPNSOCKSTREAM_H
+#define PSOCKSXX_TCPNSOCKSTREAM_H
+
+#include <psocksxx/nsockstream.h>
+
+
+namespace psocksxx {
+
+	/**
+	*   @brief TCP/IP controller class
+	*
+	*   This acts as the controller for TCP/TP communication streams.
+	*
+	*/
+	class tcpnsockstream : public nsockstream {
+
+	};
+
+} /* end of namespace psocksxx */
+
+#endif /* !PSOCKSXX_TCPNSOCKSTREAM_H */
+
