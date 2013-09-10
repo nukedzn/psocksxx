8292ff6ccc85abd245a8b6812a93787cc064a641
Uditha Atukorala <fake@thewireframe>
2013-09-10 21:45:08 +0100

    adding new socket timeout exception class

---
+++ lib/psocksxx/socktimeoutexception.cpp
@@ -0,0 +1,50 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include "socktimeoutexception.h"
+
+#include <sstream>
+
+
+namespace psocksxx {
+
+	socktimeoutexception::socktimeoutexception( const char * message ) throw() {
+		_message = message;
+	}
+
+	socktimeoutexception::socktimeoutexception( const timeval * t_val, const char * method ) throw() {
+
+		// string stream
+		std::stringstream ss;
+
+		if ( method != NULL ) {
+			ss << method << " ";
+		}
+
+		ss << "timed out (" << t_val->tv_sec << "." << t_val->tv_usec << "s)";
+
+		// update exception message
+		_message = ss.str();
+
+	}
+
+	socktimeoutexception::~socktimeoutexception() throw() { }
+
+} /* end of namespace psocksxx */
+
---
+++ lib/psocksxx/socktimeoutexception.h
@@ -0,0 +1,65 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef PSOCKSXX_SOCKTIMEOUTEXCEPTION_H
+#define PSOCKSXX_SOCKTIMEOUTEXCEPTION_H
+
+#include <psocksxx/sockexception.h>
+
+
+namespace psocksxx {
+
+	/**
+	*   @brief Socket timeout exception
+	*
+	*   This acts as the socket timeout exception class.
+	*
+	*/
+	class socktimeoutexception : public sockexception {
+	public:
+
+		/**
+		*   @brief constructor
+		*   @param message exception message
+		*
+		*   Create an timeout exception instance with the passed in exception
+		*   message.
+		*
+		*/
+		socktimeoutexception( const char * message ) throw();
+
+		/**
+		*   @brief constructor
+		*   @param t_val timed-out value
+		*   @param method action/method that timed-out
+		*
+		*   This will take the passed in timed-out value and the action
+		*   to construct the exception message.
+		*
+		*/
+		socktimeoutexception( const timeval * t_val, const char * method = NULL ) throw();
+
+		virtual ~socktimeoutexception() throw();   //!< destructor
+
+	};
+
+} /* end of namespace psocksxx */
+
+#endif /* !PSOCKSXX_SOCKTIMEOUTEXCEPTION_H */
+
