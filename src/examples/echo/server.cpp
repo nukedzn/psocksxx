7efbba9e50fa70688834f40b71410fe943d1ce75
Uditha Atukorala <ua@nuked.zone>
2015-11-27 18:49:35 +0000

    echo server/client example

--- src/examples/echo/server.cpp
+++ src/examples/echo/server.cpp
@@ -1,5 +1,27 @@
+/*
+*  psocksxx - A C++ wrapper for POSIX sockets
+*  Copyright (C) 2013-2015 Uditha Atukorala
+*
+*  This file is part of psocksxx software library.
+*
+*  psocksxx software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 
 #include <iostream>
+#include <string>
 #include <psocksxx/tcpnsockstream.h>
 
 
@@ -15,7 +37,7 @@ int main( int argc, char **argv ) {
 	try {
 		ss.bind( &naddr, true );
 	} catch( psocksxx::sockexception &e ) {
-		std::cerr <<  e.what() << std::endl;
+		std::cerr << "[server] failed to bind to socket, exception: " << e.what() << std::endl;
 		return 1;
 	}
 
@@ -23,8 +45,31 @@ int main( int argc, char **argv ) {
 	try {
 		ss.listen( 1 );
 	} catch( psocksxx::sockexception &e ) {
-		std::cerr << e.what() << std::endl;
+		std::cerr << "[server] failed to listen on socket, exception: " << e.what() << std::endl;
 		return 1;
+	}
+
+
+	// client socket stream
+	psocksxx::nsockstream * css;
+
+	// message buffer
+	std::string msg;
+
+	for (;;) {
+
+		// accept a new client
+		css = ss.accept();
+
+		(* css) >> msg;
+		std::cout << "[client] " << msg << std::endl;
+
+		// echo back
+		(* css) << msg << std::endl;
+
+		// close/cleanup client
+		delete css;
+
 	}
 
 	return 0;
